7fa4aa0cdf828e3cc2686ac9b300efa30b701487
Shuvkant <weird426344@gmail.com>
2024-07-13 04:04:33 +0545

    finished bookstore

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import { SnackbarProvider } from 'notistack'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { SnackbarProvider } from 'notistack';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
@@ -11,7 +11,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <App />
     </SnackbarProvider>
   </BrowserRouter>
-)
+);
+
 
 // ReactDOM.createRoot(document.getElementById('root')).render(
 //   <React.StrictMode>
