5d1d63fb2ae09157a51f8c1f5d621021a7fb2941
Shuvkant <weird426344@gmail.com>
2024-07-18 14:15:38 +0545

    modified axios.put to 5000

--- Frontend/src/Pages/EditBook.jsx
+++ Frontend/src/Pages/EditBook.jsx
@@ -1,55 +1,57 @@
-import React, { useState, useEffect } from 'react';
-import BackButton from '../components/BackButton';
-import Spinner from '../components/Spinner';
-import axios from 'axios';
-import { useNavigate, useParams } from 'react-router-dom';
-import { useSnackbar } from 'notistack';
+import React, { useState, useEffect } from 'react'
+import BackButton from '../components/BackButton'
+import Spinner from '../components/Spinner'
+import axios from 'axios'
+import { useNavigate, useParams } from 'react-router-dom'
+import { useSnackbar } from 'notistack'
 
 const EditBook = () => {
-  const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
-  const [publishYear, setPublishYear] = useState('');
-  const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
-  const {id} = useParams();
-  const { enqueueSnackbar } = useSnackbar();
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [publishYear, setPublishYear] = useState('')
+  const [loading, setLoading] = useState(false)
+  const navigate = useNavigate()
+  const { id } = useParams()
+  const { enqueueSnackbar } = useSnackbar()
 
   useEffect(() => {
-    setLoading(true);
-    axios.get(`http://localhost:3000/books/${id}`)
-    .then((response) => {
-        setAuthor(response.data.author);
+    setLoading(true)
+    axios
+      .get(`http://localhost:3000/books/${id}`)
+      .then((response) => {
+        setAuthor(response.data.author)
         setPublishYear(response.data.publishYear)
         setTitle(response.data.title)
-        setLoading(false);
-      }).catch((error) => {
-        setLoading(false);
-        alert('An error happened. Please Chack console');
-        console.log(error);
-      });
+        setLoading(false)
+      })
+      .catch((error) => {
+        setLoading(false)
+        alert('An error happened. Please Chack console')
+        console.log(error)
+      })
   }, [])
-  
+
   const handleEditBook = () => {
     const data = {
       title,
       author,
       publishYear,
-    };
-    setLoading(true);
+    }
+    setLoading(true)
     axios
-      .put(`http://localhost:5555/books/${id}`, data)
+      .put(`http://localhost:3000/books/${id}`, data)
       .then(() => {
-        setLoading(false);
-        enqueueSnackbar('Book Edited successfully', { variant: 'success' });
-        navigate('/');
+        setLoading(false)
+        enqueueSnackbar('Book Edited successfully', { variant: 'success' })
+        navigate('/')
       })
       .catch((error) => {
-        setLoading(false);
+        setLoading(false)
         // alert('An error happened. Please Chack console');
-        enqueueSnackbar('Error', { variant: 'error' });
-        console.log(error);
-      });
-  };
+        enqueueSnackbar('Error', { variant: 'error' })
+        console.log(error)
+      })
+  }
 
   return (
     <div className='p-4'>
