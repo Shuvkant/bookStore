9b65d28c4ea71b07ad3c2a4463895bfa26fed301
Shuvkant <weird426344@gmail.com>
2024-12-21 13:50:15 +0545

    adding proxy server

--- Frontend/src/Pages/EditBook.jsx
+++ Frontend/src/Pages/EditBook.jsx
@@ -17,7 +17,7 @@ const EditBook = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get(`http://localhost:3000/books/${id}`)
+      .get(`/books/${id}`)
       .then((response) => {
         setAuthor(response.data.author);
         setPublishYear(response.data.publishYear);
@@ -39,7 +39,7 @@ const EditBook = () => {
     };
     setLoading(true);
     axios
-      .put(`http://localhost:3000/books/${id}`, data)
+      .put(`/books/${id}`, data)
       .then(() => {
         setLoading(false);
         enqueueSnackbar("Book Edited successfully", { variant: "success" });
